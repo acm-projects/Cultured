298b0fb13c27d8d89daf65f7702d8ff02c627758
Brian Liu <brianliu1337@gmail.com>
2020-10-18 01:53:08 -0500

    initial commit to db branch

---
+++ dbserver.js
@@ -0,0 +1,23 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Brian:OhqfZtDiLn1tXHYt@cluster0.tkmjp.gcp.mongodb.net/Test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const collection = client.db("Test").collection("Brian");
+  console.log('Connected. . .')
+
+  //perform actions on the collection object
+
+  // var testObj = {status: "ok"};
+  // collection.insertOne(testObj, err=>{
+  //   if (err) throw err;
+  //   console.log("Inserted testObj")
+  // })
+
+  // console.log('Closing. . .')
+  // client.close();
+});
+
+
+
