7c693d191d0d32fb1690f63058a1e3be03dcbf36
Jordan Frimpter <afirebirdrising@gmail.com>
2020-10-24 17:04:03 -0500

    Front end integration of map complete

--- src/components/map/country-table.js
+++ src/components/map/country-table.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import CountryName from './country-name'
 import CountryHeader from './country-header'
+import CountryCol from './country-col'
 class CountryTable extends React.Component {
     //Render country name if it doesn't conflict with search and category if it isn't the same as the last category
     render() {
         const filterText = this.props.filterText;
         const rows = [];
+        var section = [];
         let lastCategory = null;
+        //for each country, places into rows
         this.props.countries.forEach((country) => {
+            //
             if (country.countryName.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+                //rows.push(<CountryCol row={section}/>);
                 return;
             }
-            if (country.location !== lastCategory) {
-                rows.push(
+            //if the current location is not the last location
+            if (country.location !== lastCategory ) {
+                //if there is a section to add to the row
+                if (section.length > 0)
+                    //add the section
+                    rows.push(<CountryCol elements={section} />);   //adds column to list
+                section = [];       //clear section array
+                section.push(       //push header
                     <CountryHeader country={country} key={this.props.countries.location} />
                 );
             }
-            rows.push(
+            //push country to section
+            section.push(
                 <CountryName country={country} key={country.countryName}/>
             );
+            //'iterates' last category
             lastCategory = country.location;
         });
+        rows.push(<CountryCol elements={section} />);   //adds column to list
+                  //cleans up row collection to catch last row
+        
         return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Countries</th>
-                        </tr>
-                    </thead>
-                    <tbody>{rows}</tbody>
-                </table>
+            
+    <div>
+                                    
+        <div className="container bg-light">                         
+            <div className="row">
+              {rows}
             </div>
+        
+        </div>
+    </div>
         );
     }
 }
--- src/components/map/search-bar.js
+++ src/components/map/search-bar.js
@@ -10,11 +10,25 @@ class SearchBar extends React.Component {
     }
     render() {
         return (
-            <div>
-                <form>
-                    <input className="searchBar" type="text" placeholder="Search..." value={this.props.filterText} onChange={this.handleFilterTextChange} />
-                </form>
+        <div  className="container align-center text-center">
+            <div className="text-light">
+                Type the name of a particular country, or hit the Visit Random button to get started!
+                <div>
+                <div className="row">
+                    <div className="col-md"/>
+                        <div className="col col-md-6">
+                            <form>
+                              <div class="form-group">
+                                <label></label>
+                                <input type="text" class="form-control" placeholder="Search Country by Name" value={this.props.filterText} onChange={this.handleFilterTextChange}/>
+                              </div>
+                            </form>
+                        </div>
+                        <div className="col-md"/>
+                    </div>
+                </div>
             </div>
+        </div>
         );
     }
 }
