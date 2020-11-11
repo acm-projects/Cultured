add381823b71ab9602de3131d698428b0d970813
Jordan Frimpter <afirebirdrising@gmail.com>
2020-11-11 16:49:16 -0600

    Updates

---
+++ precache-manifest.0d4391956e77161e57141026499ec278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f57d2faed0626073b6d6131d11bef7",
+    "url": "/Cultured/index.html"
+  },
+  {
+    "revision": "43da8b00138f2a4de2b1",
+    "url": "/Cultured/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "57af60e75b265a37d0ff",
+    "url": "/Cultured/static/css/main.b171456d.chunk.css"
+  },
+  {
+    "revision": "43da8b00138f2a4de2b1",
+    "url": "/Cultured/static/js/2.c56772e9.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Cultured/static/js/2.c56772e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57af60e75b265a37d0ff",
+    "url": "/Cultured/static/js/main.c508054e.chunk.js"
+  },
+  {
+    "revision": "1a57c46f233b6f6d8072",
+    "url": "/Cultured/static/js/runtime-main.af6e4354.js"
+  }
+]);
